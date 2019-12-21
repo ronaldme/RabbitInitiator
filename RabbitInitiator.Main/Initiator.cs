a197a0500c3b6773b84a395071e094363bdb7e3e
Ronald <ronaldme@gmail.com>
2019-12-21 12:22:08 +0100

    Add main project

---
+++ RabbitInitiator.Main/Initiator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using EasyNetQ.Management.Client;
+using EasyNetQ.Management.Client.Model;
+
+namespace RabbitInitiator.Main
+{
+    public class Initiator
+    {
+        public async Task CreateUser(string user, string password)
+        {
+            var initial = new ManagementClient("http://localhost", "guest", "guest");
+            await initial.CreateUserAsync(new UserInfo(user, password));
+        }
+    }
+}
