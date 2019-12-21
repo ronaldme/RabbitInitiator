eaa96b47b6f606d9ab8efe0cef786854c8779cd6
Ronald <ronaldme@gmail.com>
2019-12-21 12:21:15 +0100

    Add initial WPF application code with ReactiveUI

---
+++ RabbitInitiator.WPF/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Windows;
+using ReactiveUI;
+using Splat;
+
+namespace RabbitInitiator.WPF
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            Locator.CurrentMutable.RegisterViewsForViewModels(Assembly.GetCallingAssembly());
+        }
+    }
+}
---
+++ RabbitInitiator.WPF/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using ReactiveUI;
+using RabbitInitiator.WPF.ViewModels;
+
+namespace RabbitInitiator.WPF
+{
+    public partial class MainWindow : ReactiveWindow<AppViewModel>
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ViewModel = new AppViewModel();
+
+            this.WhenActivated(disposableRegistration =>
+            {
+            });
+        }
+    }
+}
