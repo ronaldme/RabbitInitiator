e9a571e37eab9d370011c6f403b3767e3d7bc25c
Ronald <ronaldme@gmail.com>
2019-12-22 20:23:55 +0100

    Add create user / vhost methods

--- RabbitInitiator.Main/Initiator.cs
+++ RabbitInitiator.Main/Initiator.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using EasyNetQ.Management.Client;
 using EasyNetQ.Management.Client.Model;
 
@@ -7,10 +6,31 @@ namespace RabbitInitiator.Main
 {
     public class Initiator
     {
+        private ManagementClient GetClient(string host = "http://localhost", string user = "guest", string password = "guest")
+            => new ManagementClient(host, user, password);
+
         public async Task CreateUser(string user, string password)
         {
-            var initial = new ManagementClient("http://localhost", "guest", "guest");
-            await initial.CreateUserAsync(new UserInfo(user, password));
+            var client = GetClient();
+            await client.CreateUserAsync(new UserInfo(user, password));
+        }
+
+        public async Task CreateUserAndVhost(string userName, string password, string vhostName)
+        {
+            var client = GetClient();
+
+            var user = await client.CreateUserAsync(new UserInfo(userName, password));
+            var vhost = await client.CreateVhostAsync(vhostName);
+            await client.CreatePermissionAsync(new PermissionInfo(user, vhost));
+        }
+
+        public async Task GiveUserVhostPermissions(string userName, string vhostName)
+        {
+            var client = GetClient();
+
+            var user = await client.GetUserAsync(userName);
+            var vhost = await client.GetVhostAsync(vhostName);
+            await client.CreatePermissionAsync(new PermissionInfo(user, vhost));
         }
     }
 }
