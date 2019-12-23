1942049f8c69619932f9d17af6ce6085c52ebbe4
Ronald <ronaldme@gmail.com>
2019-12-23 21:18:11 +0100

    Add wpf input fields / save button

--- RabbitInitiator.WPF/MainWindow.xaml.cs
+++ RabbitInitiator.WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ReactiveUI;
+﻿using System.Reactive.Disposables;
+using ReactiveUI;
 using RabbitInitiator.WPF.ViewModels;
 
 namespace RabbitInitiator.WPF
@@ -7,11 +8,30 @@ namespace RabbitInitiator.WPF
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent(); 
             ViewModel = new AppViewModel();
 
             this.WhenActivated(disposableRegistration =>
             {
+                this.Bind(ViewModel,
+                        viewModel => viewModel.User,
+                        view => view.User.Text)
+                    .DisposeWith(disposableRegistration);
+
+                this.Bind(ViewModel,
+                        viewModel => viewModel.Password,
+                        view => view.Password.Text)
+                    .DisposeWith(disposableRegistration);
+
+                this.Bind(ViewModel,
+                        viewModel => viewModel.Vhost,
+                        view => view.VHost.Text)
+                    .DisposeWith(disposableRegistration);
+
+                this.BindCommand(ViewModel,
+                        viewModel => viewModel.CreateUser,
+                        view => view.CreateUser)
+                    .DisposeWith(disposableRegistration);
             });
         }
     }
--- RabbitInitiator.WPF/ViewModels/AppViewModel.cs
+++ RabbitInitiator.WPF/ViewModels/AppViewModel.cs
@@ -1,8 +1,39 @@
-﻿using ReactiveUI;
+﻿using System.Windows.Input;
+using ReactiveUI;
 
 namespace RabbitInitiator.WPF.ViewModels
 {
     public class AppViewModel : ReactiveObject
     {
+        public AppViewModel()
+        {
+            CreateUser = ReactiveCommand.Create(() =>
+            {
+                // TODO
+            });
+        }
+
+        private string _user;
+        public string User
+        {
+            get => _user;
+            set => this.RaiseAndSetIfChanged(ref _user, value);
+        }
+
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set => this.RaiseAndSetIfChanged(ref _password, value);
+        }
+
+        private string _vhost;
+        public string Vhost
+        {
+            get => _vhost;
+            set => this.RaiseAndSetIfChanged(ref _vhost, value);
+        }
+
+        public ICommand CreateUser { get; set; }
     }
 }
