70711a31770dc376ebe29bd1e0e1740bb5d60fd0
Ronald <ronaldme@gmail.com>
2019-12-29 17:27:48 +0100

    Implement console program

--- RabbitInitiator/Program.cs
+++ RabbitInitiator/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
-using EasyNetQ.Management.Client;
-using EasyNetQ.Management.Client.Model;
+using RabbitInitiator.Main;
 
 namespace RabbitInitiator
 {
     class Program
     {
+        private static readonly Initiator initiator = new Initiator();
+
         static async Task Main(string[] args)
         {
             while (true)
@@ -28,15 +30,66 @@ namespace RabbitInitiator
                 Console.WriteLine();
             }
 
-            Console.WriteLine("Exiting program");
+            Console.WriteLine("Exiting program...");
         }
 
         private static async Task HandleMenuOptions(int val)
         {
             switch (val)
             {
+                case 1:
+                    break;
+                case 2:
+                    await CreateUser(val);
+                    break;
+                case 3:
+                    await CreateUserAndVhostPermissions(val);
+                    break;
             }
         }
+
+        private static async Task CreateUserAndVhostPermissions(int val)
+        {
+            Console.WriteLine("Enter user/password/vhost as follows: user:password:vhost");
+
+            var userPasswordAndVhost = Console.ReadLine();
+            if (userPasswordAndVhost == null || !userPasswordAndVhost.Contains(":") || userPasswordAndVhost.Length < 5)
+            {
+                await HandleMenuOptions(val);
+                return;
+            }
+
+            var splitted = userPasswordAndVhost.Split(":");
+            if (splitted.Length != 3 || splitted.Any(string.IsNullOrEmpty))
+            {
+                await HandleMenuOptions(val);
+                return;
+            }
+
+            await initiator.CreateUserAndVhost(splitted[0], splitted[1], splitted[2]);
+        }
+
+        private static async Task CreateUser(int val)
+        {
+            Console.WriteLine("Enter user/password as follows: user:password");
+
+            var userAndPass = Console.ReadLine();
+            if (userAndPass == null || !userAndPass.Contains(":") || userAndPass.Length < 3)
+            {
+                await HandleMenuOptions(val);
+                return;
+            }
+
+            var splitted = userAndPass.Split(":");
+            if (splitted.Length != 2 || splitted.Any(string.IsNullOrEmpty))
+            {
+                await HandleMenuOptions(val);
+                return;
+            }
+
+            await initiator.CreateUser(splitted[0], splitted[1]);
+        }
+
 
         private static void ShowMenu()
         {
